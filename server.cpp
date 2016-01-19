35b4084d0797b5b73ea41d28b02f2b7fd4b95904
Chris Konstad <chriskon149@gmail.com>
2016-01-19 11:08:29 -0800

    Fixing includes

--- server.cpp
+++ server.cpp
@@ -1,11 +1,11 @@
 #include "server.h"
 
-#include <stdio.h>
+//#include <stdio.h>
 #include <sys/types.h>   // definitions of a number of data types used in socket.h and netinet/in.h
 #include <sys/socket.h>  // definitions of structures needed for sockets, e.g. sockaddr
 #include <netinet/in.h>  // constants and structures needed for internet domain addresses, e.g. sockaddr_in
-#include <stdlib.h>
-#include <strings.h>
+//#include <stdlib.h>
+//#include <strings.h>
 
 #include <cerrno>
 #include <chrono>
